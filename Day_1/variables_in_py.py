e201cd1327149137559de97287947a2070566d82
Manav Mittal <manavmittal1211@gmail.com>
2026-05-05 15:09:50 +0530

    Learn: Complete variable and it's name module (# Day 1)

---
+++ Day_1/variables_in_py.py
@@ -0,0 +1,13 @@
+# Python variables
+# name = input("What is your name?")
+# print(name)
+# print(len(name))
+
+
+username = input("Please enter your name: ")
+length = len(username)
+
+print(username)
+print(length)
+
+## Coding challenge
