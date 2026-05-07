4edacc159b25aa3eeada6a1c4b4e6d81bbb703e1
Manav Mittal <manavmittal1211@gmail.com>
2026-05-07 17:27:15 +0530

    Learn: Learn about primitive data types (# Day 2)

---
+++ Day_2/primitive_data_types.py
@@ -0,0 +1,50 @@
+# Data Types
+
+# "Hello"   <-- This is a string
+# We can pull out individual characters using square brakets and
+# give the index or position of the character
+
+print("Hello"[0])
+# Programmers always start counting from 0
+# Because we work with binary
+# This method of pulling characters from string is called
+# Subscripting
+
+
+# For example to pull out the last character
+print("Hello"[4])
+# Secret trick : We can use negative indeces
+# for example, -1 <-- Signifies last character in the string
+
+# Just remember, "123" even if a number is inside a double quotes
+# and it will be considered as string
+
+
+print("123" + "345")
+# Will it be concatenated or added ?
+# The result we got is "123345", meaning it is a string and is
+# concatenated.
+
+# To add them, we have to add integers
+# Integers = Whole numbers
+# We just have to write the number without anything else to declare them
+print(123 + 345)
+
+# Large Integers
+# In some places, we keep commas so, we can use them using "underscores"
+# Underscore can be used to make the large digits more readable.
+print(123_456_7898)
+# 1234567898
+
+# What do you call the decimal number which has a period ?
+# We cann it
+# Float = Floating point number
+print(3.14179)
+# Decimal floating around the number
+
+
+# Boolean
+# It has only two values 
+# Either
+print(True)
+print(False)
