92f8a93db1ab317fa9b7ced003b04a0dc21e1a42
Manav Mittal <manavmittal1211@gmail.com>
2026-05-09 12:35:05 +0530

    learn: learn conditional statements (# Day 3)
    - See what the conditional statements like if, else are.
    - See the comparision operators are

---
+++ Day_3/condition_flow_and_operator.py
@@ -0,0 +1,16 @@
+print("Welcome to the rollercoaster!")
+user_height = int(input("What is your height in cm"))
+
+if user_height > 120:
+    print("You can ride the rollercoaster")   # Editor indents the code automatically
+else:                                         # indentation is very important in py.
+    print("Sorry you have to grow taller before you can ride")
+
+
+# Conditional Operator
+# > - Greater than
+# < - Less Than
+# >= - Greater than equals to
+# <= - Less than equals to
+# == - Equals to
+# != - Not equals to
