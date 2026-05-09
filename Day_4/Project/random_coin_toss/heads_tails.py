785f49a4831bb85e49a026d6a90bb746a02262fb
Manav Mittal <manavmittal1211@gmail.com>
2026-05-09 18:14:19 +0530

    feat: Create a random coin toss program (# Day 4)

---
+++ Day_4/Project/random_coin_toss/heads_tails.py
@@ -0,0 +1,17 @@
+# The program should produce either heads or tails
+import random
+
+# random_number  = random.randint(1, 2)
+# print(random_number)
+# if random_number % 2 == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+random_number = random.randint(0,1)
+print(random_number)
+if random_number == 0:
+    print("Heads")
+else:
+    print("Tails")
