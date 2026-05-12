8714ab6e75ffdbc24b224cfb46414ee6fe9a19ee
Manav Mittal <manavmittal1211@gmail.com>
2026-05-12 21:03:37 +0530

    Learn: Learn about while loops (# Day 6)

---
+++ Day_6/while_loops.py
@@ -0,0 +1,39 @@
+# While Loops
+
+# The loop that will keep going while a conditions is true.
+
+# For loop vs While loop
+# For loop
+# Looping through a list of items
+# using a range function
+
+
+# While loop
+# Looks like this
+'''
+while something_is_true
+    # Do something repeatedly
+    # Do this
+    # Then do this
+    # Then do this
+'''
+
+'''
+For loops are great when we wanna iterate over something and 
+we wanna do something to the thing we are iterating over
+
+While loops are great when we don't really care the sequence of the 
+number, but wanna carry out a functionality many times until a 
+certain condition becomes false
+
+While loops are dangerous than for loops because in for loops, 
+you're setting ahead of time how many times something is going to run.
+In while loop, they will continue running until a particular condition
+becomes false.
+
+If the condition never becomes false, while loops becomes infinite loops.
+'''
+
+'''
+It's helpful to check the condition in while, into the print
+'''
