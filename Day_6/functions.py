42c375b6bd30d912530ea6df183384384a139e0e
Manav Mittal <manavmittal1211@gmail.com>
2026-05-12 20:41:07 +0530

    Learn: Learn functions and indentations (# Day 6)
    - How to define own functions
    - How to call the defined functions
    - Space vs tabs for indentation, the preferred way.

---
+++ Day_6/functions.py
@@ -0,0 +1,64 @@
+# Functions
+# Defining and calling the functions
+
+# We can get our code to perform different pieces of functionality
+
+# There are a lot of built-in functions in python
+
+# We can start typing one of the functions that we've been using a lot
+#, which is the print function.
+
+# We know it's a function wiht the set of paranthesis after the name
+
+print("Hello")
+# for this functions, we can pass an argument inside, and it will be
+# printed.
+
+
+# len() calculates the length of the string here
+new_char = len("Hello")
+print(new_char)
+
+
+## These are built in functions
+
+# What if we want to make our own functions.
+# We start with the keyword `def`, because we are creating/defining
+# our function. After, the def, we define the name of the function
+# along with the parantheses. Finally, we put a colon,
+# that tells python that everything after this and indented belongs to
+# the funciton we have created.
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# As of yet, function does nothing on its own, because we haven't exe
+# -cuted it.
+# We can define lots of functions ahead of time, and when we need them
+# we can trigger them/ call the function.
+
+
+# calling the function
+my_function()
+
+
+"""
+Defining functions
+
+def my_function():
+    # Do this
+    # Then do this
+    # Finally do this
+
+# The will be carried out one we call the function
+
+# Calling the function
+my_function()
+
+
+Functions give us a way to refer to a set of instructions in a single 
+instruction.
+"""
---
+++ Day_6/indentation_in_py.py
@@ -0,0 +1,10 @@
+# Indentation in Python
+
+# Indentation is very important in python
+
+# eg
+def my_function():
+    print("Hello")
+    print("Word")
+
+# You can create indentation using tab or space
