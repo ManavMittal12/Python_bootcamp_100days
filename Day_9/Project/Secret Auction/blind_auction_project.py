ac99903c1bc1514babe38ea7b694f4a39299a85a
Manav Mittal <manavmittal1211@gmail.com>
2026-05-15 23:22:32 +0530

    proj: create a "Blind Auction" Project (# Day 9)

---
+++ Day_9/Project/Secret Auction/blind_auction_project.py
@@ -0,0 +1,31 @@
+from art import logo
+
+print(logo)
+# TODO-1: Ask the user for input
+new_bidder = True
+
+bidder_dict = {}
+while new_bidder:
+    user_name = input("Please enter your name : ")
+    user_bid = int(input("Please enter your bid : $" ))
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+
+    bidder_dict[user_name] = user_bid
+
+
+    another_bidder = input("Are there more bids ? 'Yes' or 'No' : ").casefold()
+    if another_bidder == "no":
+        new_bidder = False
+    else:
+        print("\n" * 100)
+
+# TODO-4: Compare bids in dictionary
+max_bid = float('-inf') # that's how you declare infinity
+highest_bidder = None
+for hbu in bidder_dict:
+    if bidder_dict[hbu] > max_bid:
+        max_bid = bidder_dict[hbu]
+        highest_bidder = hbu
+
+print(f"Bid won ${max_bid} by {highest_bidder}")
