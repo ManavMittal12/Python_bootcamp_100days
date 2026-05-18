c999d1a5ad842e51c945870ce3899dd025bad2d2
Manav Mittal <manavmittal1211@gmail.com>
2026-05-18 21:10:02 +0530

    learn: Learn about aliasing in python (# Day 10)

---
+++ Day_10/aliasing_in_python.py
@@ -0,0 +1,13 @@
+
+Aliasing in python
+# We can store a function as a value of a variable.
+def add(n1, n2):
+    return n1 + n2
+
+# we trigger a function using parenthesis, if we write add()
+my_fav_operation = add
+# but if we want to store a function with another name
+# , give it another reference, then we would do it like this without the parentheses, only
+# with the name of the function.
+
+my_fav_operation(2 , 3)
