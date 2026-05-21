48e5985fc62f6e80547172ac19e7c7e7d1556093
Manav Mittal <manavmittal1211@gmail.com>
2026-05-21 23:49:33 +0530

    proj: Create a "Number guessing game" (# Day 12)
    - Completed Day 12

---
+++ Day_12/Project/Number Guessing Project/Number Guessing Game.md
@@ -0,0 +1,9 @@
+# Number Guessing Project
+
+The goal is to build a guess the number game.
+
+Demo
+https://appbrewery.github.io/python-day12-demo/
+
+ASCII Art
+You can get hold of ASCII art using the link below: https://patorjk.com/software/taag/#p=display&f=Graffi
---
+++ Day_12/Project/Number Guessing Project/main.py
@@ -0,0 +1,60 @@
+from art import logo
+import random
+
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").casefold()
+    if difficulty == "easy":
+        return 10
+    elif difficulty == "hard":
+        return 5
+    return None
+
+
+def computer_chooses_number():
+    return random.randint(1, 100)
+
+def guessing_game():
+    is_game_over = False
+
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+
+    no_of_attempts = set_difficulty()
+    computer_choice = computer_chooses_number()
+
+
+
+
+    while not is_game_over:
+        print(f"You have {no_of_attempts} attempts remaining to guess the number.")
+        user_guess = int(input("Make a guess: "))
+        if user_guess == computer_choice:
+            print(f"You got it! The answer was {computer_choice}.")
+            is_game_over = True
+        elif user_guess > computer_choice:
+            print("Too High")
+            print("Guess Again")
+            no_of_attempts -= 1
+        else:
+            print("Too Low")
+            print("Guess Again")
+            no_of_attempts -= 1
+
+        if no_of_attempts == 0:
+            is_game_over = True
+            print("You've run out of guesses. Refresh the page to run again.")
+
+
+
+
+start_game = True
+
+while start_game:
+    print("\n"*1000)
+    if input("Do you want to play the Guessing game ?: 'Y' or 'N' --> ").casefold() in ("yes","y"):
+        print("\n"*1000)
+        guessing_game()
+    else:
+        start_game = False
