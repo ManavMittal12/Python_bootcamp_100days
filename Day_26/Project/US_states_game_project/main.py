3257a92cbfa1f84afc561657b975df709ae4f84d
Manav Mittal <manavmittal1211@gmail.com>
2026-06-09 17:06:23 +0530

    proj: Update the "US states" game with list comprehension (# Day 26)

---
+++ Day_26/Project/US_states_game_project/main.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+import pandas
+import turtle
+
+from pandas.core.interchange.dataframe_protocol import DataFrame
+
+from Day_25.US_STATES_GAME.main import missed_states
+
+screen = turtle.Screen()
+screen.title("US States game")
+
+
+
+
+# working with images in turtle
+# we can set turtle shape to new shape
+# we can load in an image as a new shape
+
+img = "blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
+
+us_states = pandas.read_csv("50_states.csv")
+list_of_states = us_states.state.to_list()
+
+is_game_on = True
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state's name ?").title()
+
+    if answer_state.lower() == "exit":
+        # Updated the missing states code with list comprehension
+        missed_states = [miss for miss in list_of_states if miss not in guessed_states]
+        break
+
+    if answer_state in list_of_states:
+        writer = Turtle()
+        writer.penup()
+        writer.hideturtle()
+        state_data = us_states[us_states.state == answer_state]
+        writer.goto(state_data.x.item(), state_data.y.item())
+        writer.write(state_data.state.item())
+        guessed_states.append(answer_state)
+
+      # we can use the break statement to break the flow of while loop
+
+
+
+
+
+
+missed_states_df = pandas.DataFrame(missed_states)
+missed_states_df.to_csv("states_to_learn.csv")
