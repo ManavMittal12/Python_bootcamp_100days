9dd5ee48f450bddc0065904dd8624a19be9c08e0
Manav Mittal <manavmittal1211@gmail.com>
2026-06-20 12:34:28 +0530

    proj: Create a "ISS Tracker" project (# Day 33)

--- Day_33/Project/ISS_Locator/main.py
+++ Day_33/Project/ISS_Locator/main.py
@@ -1,32 +1,61 @@
-import requests # this package is needed to call api
+import requests
+import smtplib
+from datetime import datetime
+import time
+
+MY_LAT = 27.1767
+MY_LONG = 78.0081
 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")  # we get back the response codes.
-print(response)
+EMAIL = "manavmittal1211@gmail.com"
+with open("../../../../../python_google.txt") as data1:
+    PASSWORD = data1.read()
 
 
-'''
-https://www.webfx.com/web-development/glossary/
-# 1XX - hold on response code
-# 2XX - Here you go
-# 3XX - Go away (you don't have permission)
-# 4XX - Doesn't exists or you screwed up
-# 5XX - The server screwed up, Maybe the server is down or maybe there's some other issue
-'''
+def is_iss_close():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data=response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    print(iss_latitude, iss_longitude)
+    if MY_LAT - 5 <= iss_longitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_latitude <=MY_LONG+5:
+        print("ISS Above us")
+        return True
+    else:
+        return False
 
 
-# if response.status_code != 200:
-#     raise Exception("Bad response from ISS API")    # that's very generic
+def is_night():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data=response.json()
+    parameters = {
+        "lat" : MY_LAT,
+        "lng" : MY_LONG,
+        "formatted" : 0,
+        "tzid" : "Asia/Kolkata"
+    }
+    response = requests.get(url=f"https://api.sunrise-sunset.org/json?lat={MY_LAT}&lng={MY_LONG}", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-# there are too many status codes
-# use request module to raise the exceptions and show the user.
 
-response.raise_for_status()
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-# getting hold of data
-data = response.json()["iss_position"]
-longitude = data["longitude"]
-latitude = data["latitude"]
 
-iss_position = (longitude, latitude)
-print(iss_position)
+while True:
+    if is_iss_close() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(EMAIL, PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=EMAIL,
+                msg="Subject: Look Up🌃\n\nThe ISS is above you in the sky."
+            )
+    time.sleep(60)
