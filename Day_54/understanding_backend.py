4a34aeab76401d89fbc0e2e649b71d77cb8783a6
Manav Mittal <manavmittal1211@gmail.com>
2026-08-23 22:12:34 +0530

    learn: Learn about backend (# Day 54)

---
+++ Day_54/understanding_backend.py
@@ -0,0 +1,23 @@
+# We learned about HTML, CSS, and JS
+
+# to have som functionality in a website, for example, if we are making a webapp,
+# then it's got to have a backend too. If the front end is the part that displays information
+# to the user, the backend is the part that does a lot of calculations and things
+# we have been doing with Python.
+
+# full stack = front end + back end
+# front end - html, css, js - frameworks available are angular or react
+# backend - python or js - frameworks that can be used ar node or flask
+
+
+# flask - suited for smaller and basic projects
+# django - suited for larger projects.
+
+
+# There are three component we need to understand about backend
+# 1) Client - is like a user going to a browser
+
+# 2) Server - is a powerful computer hooked up to the internet an is on 24/7. always ready to
+# receive requests on the internet.
+
+# 3) Database - Souped up spreadsheet where we store all the data of our website.
