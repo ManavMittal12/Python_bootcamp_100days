b5f3939d45fc7819ad12ad9bb623140135d600a0
Manav Mittal <manavmittal1211@gmail.com>
2026-08-29 13:42:18 +0530

    learn: Learn basics about terminal/CLI/Shell (# Day 54)

---
+++ Day_54/understand_command_line_windows.py
@@ -0,0 +1,64 @@
+# Understanding command line and terminal
+
+# terminal is also known as command line because we are typing commands one line
+# at a time in order to control our computer. The command line is also known as the shell.
+
+# what exactly is a shell ?
+# If you think about your operating system as a pistachio nut, then the kernel
+# of the actual pistachio, the thing that you are going to eat , the actual nut is the kernel,
+# and in computing the kernel refers to the actual program that interfaces with the hardware.
+
+# it's the core of the operating system.
+# now the shell of the pistachio or the shell in computing refers to the user interface for us as human to
+# interact with the kernel and in turn ith the hardware of your computer.
+
+# there are two variants of the shell
+# 1) GUI of the shell
+# 2) Command line interface.
+
+
+# a lot of things that we do with command line can be done with GUI as well, then
+# why we need to use the CLI?
+
+# CLI provides better control on the computer hardware.
+
+# pwd - present working directory command.
+# ls - list command - it will list all the files and folders in the
+# current working directory.
+
+# we can use the CLI to move around our computer.
+# using cd command
+# cd - change directory.
+
+# you can write the start of the folder and press tab, it will narrow down the possibilities where
+# you wanna go.
+
+# if we want to make a new folder, we can use
+# mkdir - make directory
+# eg . mkdir test
+
+# to create a new file, we use
+# touch nameofthefile
+
+# to delete a file,
+# we have to be in the correct directory, and we should be able to see the file
+# using ls
+# and we can use the
+# rm - remove command.
+# eg. rm main.py
+
+
+# to delete a directory, we have to navigate one step up/ out of the directory
+# we want to delete.
+
+# to navigate out, we can use
+# cd ..
+
+# to delete a folder, we use
+# rm and a flag -rf, that is recursively force.
+# it does things recursively, meaning, if there are multiple folders inside the
+# the folder we want to delete, it will delete the all.
+# if you use this command on the whole computer, it is going to be a disaster.
+# because terminal is very powerful, and make sure you are careful with it.
+
+# or we can use rmdir, remove directory
