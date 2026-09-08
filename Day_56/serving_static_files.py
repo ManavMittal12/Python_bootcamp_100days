d5587ae5e9b8ec998d473eea0aead6194285e630
Manav Mittal <manavmittal1211@gmail.com>
2026-09-08 22:43:05 +0530

    Learn: Learn about rendering static files using Flask (# Day 56)

---
+++ Day_56/serving_static_files.py
@@ -0,0 +1,28 @@
+# Trying to load the static files in challenge 1 "server.py" file.
+# The photo is not rendering. We need to learn about loading static files.
+# when we check the developer tools, we see a few 404 errors on style.css
+# and the photo.png.
+# Meaning, we are missing some stuff
+
+
+# how to make the image file to render inside the flask.
+# just like our templates need to be inside a folder called templates
+
+# Flask will look for all your static files like your images or css files
+# inside a folder called static.
+
+# after this, the image will be rendered.
+
+
+# rendering the css file in the challenge 2 folder.
+
+
+# One of the things to keep in mind is that chrome likes to cache the static
+# files
+
+# meaning, to save the internet usage, chrome likes to
+# cache the css and javascripts. Once the browser downloads it
+# it loads the same files.
+
+# We need the chrome to hard reload
+# shift + clicking on reload button.
